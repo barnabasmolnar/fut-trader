083ecd9d6e069e7e7b3943932cff77f4b75a3c3f
Barnabás Molnár <barna.molnar90@gmail.com>
2019-01-10 02:02:20 +0100

    Updates

---
+++ precache-manifest.9b8e1c0ef025172e91d6cdb999994997.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4416fa567d28de882860",
+    "url": "/fut-trader/static/js/runtime~main.4416fa56.js"
+  },
+  {
+    "revision": "7189a578544e391e6353",
+    "url": "/fut-trader/static/js/main.7189a578.chunk.js"
+  },
+  {
+    "revision": "348ccdf2f097ebb926f2",
+    "url": "/fut-trader/static/js/1.348ccdf2.chunk.js"
+  },
+  {
+    "revision": "082371dc9b178968edb9e62b9cee2a4f",
+    "url": "/fut-trader/index.html"
+  }
+];
